b889cdc79bced62aae2d48fca7ac6159535f517b
Bryan Hunt <bryan.hunt@microchip.com>
2020-01-17 17:16:22 -0700

    SPGAPPSEXT-517 Initial commit of the trust platform project

---
+++ apps/serial_number/firmware/src/main.c
@@ -0,0 +1,75 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stdio.h>
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include "cryptoauthlib.h"
+
+extern ATCAIfaceCfg atecc608a_0_init_data;
+uint8_t sernum[9];
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    ATCA_STATUS status;
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    printf("Starting Cryptoauth Serial Number Example\n");
+    
+    status = atcab_init(&atecc608a_0_init_data);
+    printf("atcab_init: %d\n", status);
+    if (ATCA_SUCCESS == status)
+    {
+        status = atcab_read_serial_number(sernum);
+        printf("atcab_read_serial_number: %d\n", status);
+    }
+    
+    atcab_release();
+    
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+

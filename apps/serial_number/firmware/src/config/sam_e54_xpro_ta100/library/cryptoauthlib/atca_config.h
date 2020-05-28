057d67f9989ec00c4c8d7ec7207df4e97e391a01
Rathan Rajagopal <rathan.chandavathrajagopal@microchip.com>
2020-05-28 17:42:38 +0530

    - SPGAPPSEXT-674 #time 30m Added serial number project for TA100 based on SAME54 Xpro

---
+++ apps/serial_number/firmware/src/config/sam_e54_xpro_ta100/library/cryptoauthlib/atca_config.h
@@ -0,0 +1,64 @@
+/* Auto-generated config file atca_config.h */
+#ifndef ATCA_CONFIG_H
+#define ATCA_CONFIG_H
+
+/* MPLAB Harmony Common Include */
+#include "definitions.h"
+
+
+#ifndef ATCA_HAL_SPI
+#define ATCA_HAL_SPI
+#endif
+
+/** Include Device Support Options */
+#define ATCA_TA100_SUPPORT
+
+
+/* Polling Configuration Options  */
+#ifndef ATCA_POLLING_INIT_TIME_MSEC
+#define ATCA_POLLING_INIT_TIME_MSEC       1
+#endif
+#ifndef ATCA_POLLING_FREQUENCY_TIME_MSEC
+#define ATCA_POLLING_FREQUENCY_TIME_MSEC  2
+#endif
+#ifndef ATCA_POLLING_MAX_TIME_MSEC
+#define ATCA_POLLING_MAX_TIME_MSEC        2500
+#endif
+
+/** Define if the library is not to use malloc/free */
+#ifndef ATCA_NO_HEAP
+#define ATCA_NO_HEAP
+#endif
+
+#define atca_delay_ms   hal_delay_ms
+#define atca_delay_us   hal_delay_us
+
+/* \brief How long to wait after an initial wake failure for the POST to
+ *         complete.
+ * If Power-on self test (POST) is enabled, the self test will run on waking
+ * from sleep or during power-on, which delays the wake reply.
+ */
+#ifndef ATCA_POST_DELAY_MSEC
+#define ATCA_POST_DELAY_MSEC 25
+#endif
+
+
+/* Define generic interfaces to the processor libraries */
+
+typedef bool (* atca_spi_plib_read)( void * , size_t );
+typedef bool (* atca_spi_plib_write)( void *, size_t );
+typedef bool (* atca_spi_plib_is_busy)( void );
+typedef void (* atca_spi_plib_select)(uint32_t pin, bool value);
+
+typedef struct atca_plib_spi_api
+{
+    atca_spi_plib_read              read;
+    atca_spi_plib_write             write;
+    atca_spi_plib_is_busy           is_busy;
+    atca_spi_plib_select            select;
+} atca_plib_spi_api_t;
+
+extern atca_plib_spi_api_t sercom4_plib_spi_api;
+
+
+#endif // ATCA_CONFIG_H
---
+++ apps/serial_number/firmware/src/config/sam_e54_xpro_ta100/library/cryptoauthlib/hal/TA100_0.c
@@ -0,0 +1,20 @@
+/*
+ * Code generated from MPLAB Harmony.
+ *
+ * This file will be overwritten when reconfiguring your MPLAB Harmony project.
+ * Please copy examples or other code you want to keep to a separate file or main.c
+ * to avoid loosing it when reconfiguring.
+ */
+
+#include "cryptoauthlib.h"
+
+ATCAIfaceCfg ta100_0_init_data = {
+    .iface_type            = ATCA_SPI_IFACE,
+    .devtype               = TA100,
+    .atcaspi.bus           = 0,
+    .atcaspi.select_pin    = PORT_PIN_PB28,
+    .atcaspi.baud          = 1000000,
+    .wake_delay            = 1500,
+    .rx_retries            = 20,
+    .cfg_data              = &sercom4_plib_spi_api
+};
